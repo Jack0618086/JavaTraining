d554d8fcf35bc481e7c479a202e3a1530d1419b5
Jack Chen <dc2875179@gmail.com>
2024-12-12 18:00:11 +0800

    chapter 5-2 continue(06:00)

---
+++ src/main/java/com/jack/many/Tester.java
@@ -0,0 +1,63 @@
+package com.jack.many;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class Tester {
+    public static void main(String[] args) {
+//        set();
+//        list();
+//        arrayTest();
+    }
+
+    private static void set() {
+        // set => 適合放不重複性的資料，並且無索引值所以無法取得單一資料
+        HashSet<Integer> set = new HashSet<>();
+        set.add(5);
+        set.add(8);
+        set.add(3);
+        set.add(1);
+        System.out.println(set);
+        for (int n : set) {
+            System.out.println(n);
+        }
+    }
+
+    private static void list() {
+        // List 的特性 => 順序 / 索引值 / 可重複
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(9);
+        list.add(11);
+        System.out.println(list);
+        int n1 = list.get(0);
+        int n2 = list.get(1);
+        int n3 = list.get(2);
+        int n4 = list.get(3);
+        System.out.println(list.size()); // 取得元素數量
+        List<Integer> scores = Arrays.asList(10, 20, 30, 40, 50, 60, 70);
+        for (int score : scores) {
+            System.out.println(score);
+        }
+    }
+
+    private static void arrayTest() {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[4] = 2;
+
+        int[] scores = {60, 65, 70, 75, 80};
+        System.out.println(scores); // 印出來的是記憶體位置
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(scores[i]);
+        }
+        for (int b : scores){
+            System.out.println(b);
+        }
+    }
+}
